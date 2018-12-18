dee08193a0385ff6a489355e58037691cedd9dc0
Qs-F <qsf@de-liker.com>
2018-12-18 11:52:49 +0900

    Add build

--- lib/herb/build.go
+++ lib/herb/build.go
@@ -4,6 +4,12 @@ package herb
 
 import "go/build"
 
+type BuildOption struct{}
+
 func getGOPATH() string {
 	return build.Default.GOPATH
+}
+
+func (r *Repository) Build(bo *BuildOption) ([]byte, error) {
+	return []byte{}, nil
 }
--- lib/herb/build_test.go
+++ lib/herb/build_test.go
@@ -1,3 +1,12 @@
 // copyright 2018 de-liKeR, CreatorQsF
 
 package herb
+
+import "testing"
+
+func TestBuild(t *testing.T) {
+	testquery := "github.com/golang/example/hello"
+	repo, err := Get(testquery)
+	b, err := repo.Build(nil)
+	t.Log(b, err)
+}
